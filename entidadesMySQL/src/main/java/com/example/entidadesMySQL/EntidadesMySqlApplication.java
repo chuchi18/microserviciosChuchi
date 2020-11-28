9cea0fdc9f1d37381bc312880458768c346e73f6
JesusMartin <jesusmart8@hotmail.com>
2020-11-28 16:37:54 +0100

    Añadido proyecto EntidadesMySQL

---
+++ entidadesMySQL/src/main/java/com/example/entidadesMySQL/EntidadesMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.entidadesMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesMySqlApplication.class, args);
+	}
+
+}
---
+++ entidadesMySQL/src/test/java/com/example/entidadesMySQL/EntidadesMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entidadesMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
