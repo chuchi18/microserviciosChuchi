fadb89d65bffc2b3b4b49acaef9cd31a5537a02c
JesusMartin <jesusmart8@hotmail.com>
2020-11-28 17:07:28 +0100

    Añadido proyecto entidadesMySQL con correcto grupoID

---
+++ entidadesMySQL/src/main/java/com/chuchi/microservicios/entidadesMySQL/EntidadesMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.entidadesMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesMySqlApplication.class, args);
+	}
+
+}
---
+++ entidadesMySQL/src/test/java/com/chuchi/microservicios/entidadesMySQL/EntidadesMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.entidadesMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
