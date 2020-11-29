2570a1d45686f332adddad7e60e1b6406dcc7eb8
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 22:38:40 +0100

    Añadido turbine dashboard

--- turbine/src/main/java/com/chuchi/microservicios/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/chuchi/microservicios/turbine/TurbineApplication.java
@@ -1,16 +1,17 @@
-package com.chuchi.microservicios.dashboard;
+package com.chuchi.microservicios.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
 @EnableHystrixDashboard
-public class DashboardApplication {
+public class TurbineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DashboardApplication.class, args);
+		SpringApplication.run(TurbineApplication.class, args);
 	}
 
 }
---
+++ turbine/src/test/java/com/chuchi/microservicios/turbine/TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
