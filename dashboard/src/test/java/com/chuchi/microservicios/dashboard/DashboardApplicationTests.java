4c106af63829545bae3842d2e51dffe32a6264dd
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 21:42:58 +0100

    Añadido Hystrix Dashboard microservice

---
+++ dashboard/src/test/java/com/chuchi/microservicios/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
