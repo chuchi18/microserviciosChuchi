0f36f12770dc15b9034d126ec7a76386f3064e7c
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 17:35:05 +0100

    Añadidos proyectos Pagos y Facturas

---
+++ facturas/src/test/java/com/chuchi/microservicios/facturas/FacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
