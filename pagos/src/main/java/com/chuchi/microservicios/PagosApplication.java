1c3d5226406dbbe6bfd3b8726ff3fc698099d001
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 18:43:49 +0100

    Cambiado nombre del microservicio de facturas

--- pagos/src/main/java/com/chuchi/microservicios/PagosApplication.java
+++ pagos/src/main/java/com/chuchi/microservicios/PagosApplication.java
@@ -1,4 +1,4 @@
-package com.chuchi.microservicios.pagos;
+package com.chuchi.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
