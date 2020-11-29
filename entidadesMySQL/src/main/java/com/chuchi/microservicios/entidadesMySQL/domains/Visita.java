0882a35eb2781b46bda1f246aac6506c7ec0fc73
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 16:32:35 +0100

    Añadido POJO visita

--- entidadesMySQL/src/main/java/com/chuchi/microservicios/entidadesMySQL/domains/Visita.java
+++ entidadesMySQL/src/main/java/com/chuchi/microservicios/entidadesMySQL/domains/Visita.java
@@ -1,4 +1,79 @@
 package com.chuchi.microservicios.entidadesMySQL.domains;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="visits")
 public class Visita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    @Column(name = "status")
+    protected String status;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern="yyyy/MM/dd")
+    @Column(name="visit_date")
+    private Date visit_date;
+
+    @Column(name = "description")
+    protected String description;
+
+    @Column(name = "total")
+    protected Integer total;
+
+    @Column(name = "client_id")
+    protected Integer client_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getVisit_date() {
+        return visit_date;
+    }
+
+    public void setVisit_date(Date visit_date) {
+        this.visit_date = visit_date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(Integer client_id) {
+        this.client_id = client_id;
+    }
 }
