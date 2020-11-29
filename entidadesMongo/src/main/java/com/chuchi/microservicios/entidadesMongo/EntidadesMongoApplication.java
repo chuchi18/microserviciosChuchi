b7b6e13ad9f92a3b047c645213a03cb3eec6967f
JesusMartin <jesusmart8@hotmail.com>
2020-11-29 17:12:42 +0100

    Añadido proyecto entidades Mongo

---
+++ entidadesMongo/src/main/java/com/chuchi/microservicios/entidadesMongo/EntidadesMongoApplication.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.entidadesMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesMongoApplication.class, args);
+	}
+
+}
---
+++ entidadesMongo/src/test/java/com/chuchi/microservicios/entidadesMongo/EntidadesMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchi.microservicios.entidadesMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
